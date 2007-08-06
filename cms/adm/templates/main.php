dfd5b6eb869db9877cb71180215e76ee0c4c9960
Yar Kravtsov <yarlson@gmail.com>
2007-08-06 12:23:19 +0000

    *** empty log message ***

--- cms/adm/templates/main.php
+++ cms/adm/templates/main.php
@@ -8,7 +8,7 @@
 
 <body>
 <div id="header">
-<div><?php echo __("Current user") ?>: <strong><a href="?chid=9&action=edit&id=<?php echo $this->user_id ?>"><?php echo $ref->user ?></a></strong>&nbsp; &nbsp;<a href="?action=logout" style="font-size: 11px;"><img src="i/logout.gif" alt="" width="16" height="16" hspace="6" border="0" align="absmiddle"><?php echo __("Log out") ?></a></div>
+<div><?php echo __("Current user") ?>: <strong><a href="?chid=7&action=edit&id=<?php echo $this->user_id ?>"><?php echo $ref->user ?></a></strong>&nbsp; &nbsp;<a href="?action=logout" style="font-size: 11px;"><img src="i/logout.gif" alt="" width="16" height="16" hspace="6" border="0" align="absmiddle"><?php echo __("Log out") ?></a></div>
 <?php if ($ref->id>1) { ?><a href="./"><?php } ?><img src="i/logo.gif" alt="" width="101" height="44" border="0"><?php if ($ref->id>1) { ?></a><?php } ?>
 </div>
 <div id="container">
