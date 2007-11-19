3fe80eb725906b6888379540ceb1885b342b4e1f
Guo Qiang <aqiang@gmail.com>
2007-11-19 10:07:43 +0000

    added slash("\") for news template

--- cms/adm/templates/subscribeMain.php
+++ cms/adm/templates/subscribeMain.php
@@ -1,28 +1,28 @@
-<script language="JavaScript">
+<script language=\"JavaScript\">
 function submit_delete()	{
 	return confirm('<?php echo __("Delete selected record?") ?>')
 }
 </script>
-<table width="100%" class="noborder">
+<table width=\"100%\" class=\"noborder\">
 	<tr>
-		<td width="100%" style="padding-left: 0px;"><h3 style="margin: 0px;"><?php echo __("Mailing lists") ?></h3></td>
+		<td width=\"100%\" style=\"padding-left: 0px;\"><h3 style=\"margin: 0px;\"><?php echo __("Mailing lists") ?></h3></td>
 		<td>
-<ul style="list-style-type: none;font-size: 11px;">
-	<li><a href="?chid=$this->chid&action=show_AllUsers"><img src="i/subs_users.gif" alt="" width="16" height="16" border="0" hspace="5" align="middle"><?php echo __("Subscribers") ?><img src="i/rarr.gif" alt="" hspace="3" align="middle"></a></li>
-	<li><a href="?chid=$this->chid&action=show_Conf"><img src="i/subs_options.gif" alt="" width="16" height="16" border="0" hspace="5" align="middle"><?php echo __("Preferences") ?><img src="i/rarr.gif" alt="" hspace="3" align="middle"></a></li>
+<ul style=\"list-style-type: none;font-size: 11px;\">
+	<li><a href=\"?chid=$this->chid&action=show_AllUsers\"><img src=\"i/subs_users.gif\" alt=\"\" width=\"16\" height=\"16\" border=\"0\" hspace=\"5\" align=\"middle\"><?php echo __("Subscribers") ?><img src=\"i/rarr.gif\" alt=\"\" hspace=\"3\" align=\"middle\"></a></li>
+	<li><a href=\"?chid=$this->chid&action=show_Conf\"><img src=\"i/subs_options.gif\" alt=\"\" width=\"16\" height=\"16\" border=\"0\" hspace=\"5\" align=\"middle\"><?php echo __("Preferences") ?><img src=\"i/rarr.gif\" alt=\"\" hspace=\"3\" align=\"middle\"></a></li>
 </ul></td>
 	</tr>
 </table>
-<p><a href="?chid=$chid&action=add"><img src="i/add.gif" alt="<?php echo __("Add new") ?>" width="16" height="16" border="0" hspace="3" align="absmiddle"><?php echo __("Add new") ?></a></p>
+<p><a href=\"?chid=$chid&action=add\"><img src=\"i/add.gif\" alt=\"<?php echo __("Add new") ?>\" width=\"16\" height=\"16\" border=\"0\" hspace=\"3\" align=\"absmiddle\"><?php echo __("Add new") ?></a></p>
 <style>
 ul {margin: 0px;padding-left: 20px;}
 </style>
-<table cellspacing="0" cellpadding="5" width="100%">
+<table cellspacing=\"0\" cellpadding=\"5\" width=\"100%\">
 <tr>
-	<th width="5%"><?php echo __("#") ?></td>
-	<th width="60%"><?php echo __("Subject") ?></td>
-	<th width="20%"><?php echo __("Period") ?></td>
-	<th width="15%"><?php echo __("Action") ?></td>
+	<th width=\"5%\"><?php echo __("#") ?></td>
+	<th width=\"60%\"><?php echo __("Subject") ?></td>
+	<th width=\"20%\"><?php echo __("Period") ?></td>
+	<th width=\"15%\"><?php echo __("Action") ?></td>
 </tr>
 $subscribeTR
 </table>
--- cms/adm/templates/subscribeNav.php
+++ cms/adm/templates/subscribeNav.php
@@ -1,7 +1,7 @@
-<table id="nav">
+<table id=\"nav\">
 <tr>
-	<td$class[Messages]><a href="?chid=$this->chid&action=show_Messages&id=$this->id""><?php echo __("Messages") ?></a></td>
-	<td$class[Users]><a href="?chid=$this->chid&action=show_Users&id=$this->id"><?php echo __("Subscribers") ?></a></td>
-	<td$class[Info]><a href="?chid=$this->chid&action=show_Info&id=$this->id"><?php echo __("Preferences") ?></a></td>
+	<td$class[Messages]><a href=\"?chid=$this->chid&action=show_Messages&id=$this->id\"><?php echo __("Messages") ?></a></td>
+	<td$class[Users]><a href=\"?chid=$this->chid&action=show_Users&id=$this->id\"><?php echo __("Subscribers") ?></a></td>
+	<td$class[Info]><a href=\"?chid=$this->chid&action=show_Info&id=$this->id\"><?php echo __("Preferences") ?></a></td>
 </tr>
 </table>
