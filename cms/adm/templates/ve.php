927e1ee3356afabd02767996a5f0d24d2a2a58ee
Guo Qiang <aqiang@gmail.com>
2007-11-21 06:16:02 +0000

    *** empty log message ***

--- cms/adm/templates/ve.php
+++ cms/adm/templates/ve.php
@@ -3,40 +3,50 @@
 <html>
 <head>
 	<title><?php echo __("Visual Editor")." ".__("powered by")." tinyMCE"  ?></title>
-<script language="javascript" type="text/javascript" src="/adm/tinymce/jscripts/tiny_mce/tiny_mce_gzip.php"></script>
+<script language="javascript" type="text/javascript" src="/adm/tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
+<script language="javascript" type="text/javascript" src="/adm/tinymce/jscripts/tiny_mce/tiny_mce_gzip.js"></script>
+<script type="text/javascript">
+tinyMCE_GZ.init({
+    plugins : 'style,layer,table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,media,'+ 
+		'searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras',
+    themes : 'simple,advanced',
+    languages : 'en',
+    disk_cache : true,
+    debug : false
+});
+</script>
 <script language="javascript" type="text/javascript">
-
-// Notice: The simple theme does not use all options some of them are limited to the advanced theme
 	tinyMCE.init({
+		// General options
 		mode : "textareas",
 		theme : "advanced",
-		plugins : "table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,zoom,flash,searchreplace,print,contextmenu,paste,directionality,fullscreen",
-		theme_advanced_buttons1_add_before : "save,newdocument,separator",
-		theme_advanced_buttons1_add : "fontselect,fontsizeselect",
-		theme_advanced_buttons2_add : "separator,insertdate,inserttime,preview,separator,forecolor,backcolor",
-		theme_advanced_buttons2_add_before: "cut,copy,paste,pastetext,pasteword,separator,search,replace,separator",
-		theme_advanced_buttons3_add_before : "tablecontrols,separator",
-		theme_advanced_buttons3_add : "emotions,iespell,flash,advhr,separator,print,separator,ltr,rtl,separator,fullscreen",
+		plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+
+		// Theme options
+		theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+		theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+		theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+		theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
 		theme_advanced_toolbar_location : "top",
 		theme_advanced_toolbar_align : "left",
 		theme_advanced_path_location : "bottom",
-	    plugin_insertdate_dateFormat : "%Y-%m-%d",
-	    plugin_insertdate_timeFormat : "%H:%M:%S",
-		extended_valid_elements : "img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
-		external_link_list_url : "example_data/example_link_list.js",
-		external_image_list_url : "example_data/example_image_list.js",
-		flash_external_list_url : "example_data/example_flash_list.js",
-		file_browser_callback : "mcFileManager.filebrowserCallBack",
-		paste_auto_cleanup_on_paste : true,
-		paste_convert_headers_to_strong : true,
-		language : "<?php echo __("en") ?>"
+		theme_advanced_resizing : true,
+
+		// Example content CSS (should be your site CSS)
+		content_css : "css/content.css",
+
+		// Drop lists for link/image/media/template dialogs
+		template_external_list_url : "lists/template_list.js",
+		external_link_list_url : "lists/link_list.js",
+		external_image_list_url : "lists/image_list.js",
+		media_external_list_url : "lists/media_list.js",
+
+		// Replace values for the template plugin
+		template_replace_values : {
+			username : "Some User",
+			staffid : "991234"
+		}
 	});
-	//alert(opener.document.FORMPOST.elements["<?php echo $ref ?>"].value);
-function mysave() {
-	opener.document.FORMPOST.elements["<?php echo $ref ?>"].value=document.FORMPOST.elements["text"].value;
-	window.close();
-	return false;
-}
 </script>
 </head>
 
